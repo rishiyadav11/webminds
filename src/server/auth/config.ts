74faba7d7dd5c0aa1fceb993425d9ac32bb5de82
rishiyadav11 <photonics54yadav@gmail.com>
2025-09-23 16:48:16 +0530

    added extra github account choose 2r

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -29,6 +29,7 @@ export const authConfig: NextAuthConfig = {
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      authorization: { params: { prompt: "login" } }, // 👈 Force re-prompt every time
       profile(profile) {
         return {
           id: profile.id.toString(),
